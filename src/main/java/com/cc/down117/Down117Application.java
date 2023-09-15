86c381d4d6bd8ec3980b930f2113b00bf4d06ddf
VAYO <vayo@88.com>
2023-09-15 13:17:15 +0800

    first

---
+++ src/main/java/com/cc/down117/Down117Application.java
@@ -0,0 +1,13 @@
+package com.cc.down117;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Down117Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Down117Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cc/down117/Down117ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.down117;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Down117ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
