8af2219ae2378e77ad6d5d9e801c2d175149fe15
VAYO <vayo@88.com>
2023-09-23 10:08:33 +0800

    单元测试

--- src/test/java/com/cc/down117/Down117ApplicationTests.java
+++ src/test/java/com/cc/down117/Down117ApplicationTests.java
@@ -3,11 +3,27 @@ package com.cc.down117;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class Down117ApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void teste(){
+        String str = """
+                nihao
+                wobuhao
+                """;
+        System.out.println(str);
     }
+
+
+
+    @Test
+    void contextLoads() throws InterruptedException {
+        long l = System.currentTimeMillis();
+
+        Thread.sleep(1000);
+        System.out.println((System.currentTimeMillis() - l));
+    }
+
 
 }
